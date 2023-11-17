57b93e166193898e5a989f9731c77bd00e2b3ec6
nikola01pz <nikola01pz@gmail.com>
2023-11-17 12:59:40 +0100

    add first lab exercise
    introduction to Xilinx Platform Studio

---
+++ lv1/SDK/lv1_app/Debug/src/helloworld.d
@@ -0,0 +1,36 @@
+src/helloworld.d: ../src/helloworld.c ../src/platform.h \
+ ../src/platform_config.h \
+ ../../lv1_app_bsp/microblaze_0/include/xparameters.h \
+ ../../lv1_app_bsp/microblaze_0/include/xgpio.h \
+ ../../lv1_app_bsp/microblaze_0/include/xil_types.h \
+ ../../lv1_app_bsp/microblaze_0/include/xil_assert.h \
+ ../../lv1_app_bsp/microblaze_0/include/xstatus.h \
+ ../../lv1_app_bsp/microblaze_0/include/xbasic_types.h \
+ ../../lv1_app_bsp/microblaze_0/include/xgpio_l.h \
+ ../../lv1_app_bsp/microblaze_0/include/xil_io.h \
+ ../../lv1_app_bsp/microblaze_0/include/mb_interface.h \
+ ../../lv1_app_bsp/microblaze_0/include/xil_printf.h
+
+../src/platform.h:
+
+../src/platform_config.h:
+
+../../lv1_app_bsp/microblaze_0/include/xparameters.h:
+
+../../lv1_app_bsp/microblaze_0/include/xgpio.h:
+
+../../lv1_app_bsp/microblaze_0/include/xil_types.h:
+
+../../lv1_app_bsp/microblaze_0/include/xil_assert.h:
+
+../../lv1_app_bsp/microblaze_0/include/xstatus.h:
+
+../../lv1_app_bsp/microblaze_0/include/xbasic_types.h:
+
+../../lv1_app_bsp/microblaze_0/include/xgpio_l.h:
+
+../../lv1_app_bsp/microblaze_0/include/xil_io.h:
+
+../../lv1_app_bsp/microblaze_0/include/mb_interface.h:
+
+../../lv1_app_bsp/microblaze_0/include/xil_printf.h:
---
+++ lv1/SDK/lv1_app/Debug/src/platform.d
@@ -0,0 +1,22 @@
+src/platform.d: ../src/platform.c \
+ ../../lv1_app_bsp/microblaze_0/include/xparameters.h \
+ ../../lv1_app_bsp/microblaze_0/include/xil_cache.h \
+ ../../lv1_app_bsp/microblaze_0/include/mb_interface.h \
+ ../../lv1_app_bsp/microblaze_0/include/xbasic_types.h \
+ ../../lv1_app_bsp/microblaze_0/include/xil_types.h \
+ ../../lv1_app_bsp/microblaze_0/include/xparameters.h \
+ ../src/platform_config.h
+
+../../lv1_app_bsp/microblaze_0/include/xparameters.h:
+
+../../lv1_app_bsp/microblaze_0/include/xil_cache.h:
+
+../../lv1_app_bsp/microblaze_0/include/mb_interface.h:
+
+../../lv1_app_bsp/microblaze_0/include/xbasic_types.h:
+
+../../lv1_app_bsp/microblaze_0/include/xil_types.h:
+
+../../lv1_app_bsp/microblaze_0/include/xparameters.h:
+
+../src/platform_config.h:
