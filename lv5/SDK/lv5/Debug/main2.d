6f3f944d88e0e67fb2c7ef095563c3dbe2cf9631
nikola01pz <nikola01pz@gmail.com>
2023-12-14 12:03:06 +0100

    add solved fith lab exercise

---
+++ lv5/SDK/lv5/Debug/main2.d
@@ -0,0 +1,52 @@
+main2.d: ../main2.c \
+ ../../Projekt_bsp/microblaze_0/include/xil_exception.h \
+ ../../Projekt_bsp/microblaze_0/include/xil_types.h \
+ ../../Projekt_bsp/microblaze_0/include/xstatus.h \
+ ../../Projekt_bsp/microblaze_0/include/xbasic_types.h \
+ ../../Projekt_bsp/microblaze_0/include/xparameters.h \
+ ../../Projekt_bsp/microblaze_0/include/xps2.h \
+ ../../Projekt_bsp/microblaze_0/include/xil_assert.h \
+ ../../Projekt_bsp/microblaze_0/include/xstatus.h \
+ ../../Projekt_bsp/microblaze_0/include/xps2_l.h \
+ ../../Projekt_bsp/microblaze_0/include/xil_io.h \
+ ../../Projekt_bsp/microblaze_0/include/mb_interface.h \
+ ../../Projekt_bsp/microblaze_0/include/xil_printf.h \
+ ../../Projekt_bsp/microblaze_0/include/xintc.h \
+ ../../Projekt_bsp/microblaze_0/include/xparameters.h \
+ ../../Projekt_bsp/microblaze_0/include/xintc_l.h \
+ ../../Projekt_bsp/microblaze_0/include/xgpio.h \
+ ../../Projekt_bsp/microblaze_0/include/xgpio_l.h
+
+../../Projekt_bsp/microblaze_0/include/xil_exception.h:
+
+../../Projekt_bsp/microblaze_0/include/xil_types.h:
+
+../../Projekt_bsp/microblaze_0/include/xstatus.h:
+
+../../Projekt_bsp/microblaze_0/include/xbasic_types.h:
+
+../../Projekt_bsp/microblaze_0/include/xparameters.h:
+
+../../Projekt_bsp/microblaze_0/include/xps2.h:
+
+../../Projekt_bsp/microblaze_0/include/xil_assert.h:
+
+../../Projekt_bsp/microblaze_0/include/xstatus.h:
+
+../../Projekt_bsp/microblaze_0/include/xps2_l.h:
+
+../../Projekt_bsp/microblaze_0/include/xil_io.h:
+
+../../Projekt_bsp/microblaze_0/include/mb_interface.h:
+
+../../Projekt_bsp/microblaze_0/include/xil_printf.h:
+
+../../Projekt_bsp/microblaze_0/include/xintc.h:
+
+../../Projekt_bsp/microblaze_0/include/xparameters.h:
+
+../../Projekt_bsp/microblaze_0/include/xintc_l.h:
+
+../../Projekt_bsp/microblaze_0/include/xgpio.h:
+
+../../Projekt_bsp/microblaze_0/include/xgpio_l.h:
---
+++ lv5/SDK/lv5/Debug/subdir.mk
@@ -0,0 +1,24 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../main2.c 
+
+OBJS += \
+./main2.o 
+
+C_DEPS += \
+./main2.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+%.o: ../%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: MicroBlaze gcc compiler'
+	mb-gcc -Wall -O0 -g3 -c -fmessage-length=0 -I../../Projekt_bsp/microblaze_0/include -mxl-barrel-shift -mxl-pattern-compare -mcpu=v8.50.c -mno-xl-soft-mul -Wl,--no-relax -ffunction-sections -fdata-sections -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
